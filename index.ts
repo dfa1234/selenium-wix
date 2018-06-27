063bc9644e279f7a02760ab65549a53afb706d76
David <dfaure@outlook.com>
2018-06-27 17:08:20 +0300

    generic use for next time

--- index.ts
+++ index.ts
@@ -1,18 +1,14 @@
-import {COUPONS} from "./datas/coupons";
-
 require('chromedriver');
 import {By, Key, until} from  'selenium-webdriver';
 import * as webdriver from 'selenium-webdriver';
-import {LOGIN_WIX, PASSWORD_WIX, PRICE} from "./config";
-import {COOKIES} from "./datas/cookies";
+
+import {COOKIES, COUPONS, LOGIN_WIX, PASSWORD_WIX, PRICE} from "./config";
 
 // const couponType = process.argv[2];
 // if(!couponType){
 //     console.log('need coupon type');
 //     process.exit(1);
 // }
-let mCOUPONS = COUPONS;
-
 
 (async function example() {
 
@@ -26,9 +22,10 @@ let mCOUPONS = COUPONS;
         await driver.findElement(By.id('input_1')).sendKeys(PASSWORD_WIX, Key.RETURN);
         await driver.findElement(By.css('.login-btn')).click();
         console.log('Human: you need to resolve captcha and click Sign in!');
-        driver.manage().getCookies().then(
-            result => console.log(result)
-        );
+        await driver.wait(until.urlContains('dashboard'), 600000);
+        let COOKIES = await driver.manage().getCookies();
+        console.log('Hooray! Keep yours cookies for next time: ',COOKIES);
+        //TODO save in file ourselves, and add a failback on reconnect which use this login function and go back to reconnect
     }
 
     async function reconnect(){
@@ -44,19 +41,22 @@ let mCOUPONS = COUPONS;
         await driver.sleep(5000);
     }
 
-    while(true) {
+    let done = false;
+
+    while(!done) {
         try {
 
             //no longer need if we have the cookies
+            //for displaying the cookies we need to use it on the 1st time only
             //await login();
 
             await reconnect();
 
             //We finally entered to the console!
 
-            for(let KEY of mCOUPONS){
+            for(let COUPON_KEY of COUPONS){
 
-                console.log(KEY);
+                console.log(COUPON_KEY);
 
                 await driver.wait(until.elementIsVisible(driver.findElement(By.className("wix-style-svg-font-icons-plus"))));
                 await driver.sleep(100);
@@ -64,11 +64,11 @@ let mCOUPONS = COUPONS;
                 await driver.wait(until.elementIsVisible(driver.findElement(By.id("couponCode"))));
                 await driver.findElement(By.id("couponCode")).click();
                 await driver.findElement(By.id("couponCode")).clear();
-                await driver.findElement(By.id("couponCode")).sendKeys(KEY);
+                await driver.findElement(By.id("couponCode")).sendKeys(COUPON_KEY);
                 await driver.sleep(100);
                 await driver.findElement(By.id("couponName")).click();
                 await driver.findElement(By.id("couponName")).clear();
-                await driver.findElement(By.id("couponName")).sendKeys(KEY);
+                await driver.findElement(By.id("couponName")).sendKeys(COUPON_KEY);
                 await driver.sleep(100);
                 await driver.findElement(By.id("couponDiscountValue")).click();
                 await driver.findElement(By.id("couponDiscountValue")).clear();
@@ -83,14 +83,14 @@ let mCOUPONS = COUPONS;
 
             }
 
+            done = true;
 
-        } catch(e){
+        } catch(e) {
             console.log(e)
-        }finally {
+        } finally {
+            //to close the window on
             //await driver.quit();
         }
     }
-
-
 
   })();
